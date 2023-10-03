0ffbe6102b7b29a8857d2209135965e97cd84a3b
ViacheslavGbc <Viacheslav.Nepomniashchyi@georgebrown.ca>
2023-10-02 20:54:12 -0400

    assignment 1 v1

--- src/main.cpp
+++ src/main.cpp
@@ -203,48 +203,22 @@ int main()
         0, 1, 3,  // first Triangle
         1, 2, 3   // second Triangle
     };
-    unsigned int VBO, VAO, EBO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
-    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
-    glBindVertexArray(VAO);
 
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(verticec), verticec, GL_STATIC_DRAW);
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
-    // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-    // remember: do NOT unbind the EBO while a VAO is active as the bound element buffer object IS stored in the VAO; keep the EBO bound.
-    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-
-    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
-    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
-    glBindVertexArray(0);
-
-
-    // uncomment this call to draw in wireframe polygons.
-    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-
+    unsigned int ;
     
-
-
+    
     // Generate vertex buffers (data)
     // Generate vertex arrays (collections + descriptions of vertex buffers)
-    GLuint vaoRainbow, vaoWhite, vaoRed, vaoBlue, vaoBlack;
+    GLuint vaoRainbow, vaoWhite, vaoRed, vaoBlue, vaoBlack,VBO, VAO, EBO;
     GLuint vboPos, vboRainbow, vboWhite, vboRed, vboBlue, vboBlack;
     glGenVertexArrays(1, &vaoRainbow);
     glGenVertexArrays(1, &vaoWhite);
     glGenVertexArrays(1, &vaoRed);
     glGenVertexArrays(1, &vaoBlue);
     glGenVertexArrays(1, &vaoBlack);
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
     glGenBuffers(1, &vboPos);
     glGenBuffers(1, &vboBlack);
     glGenBuffers(1, &vboRainbow);
@@ -275,6 +249,29 @@ int main()
     // Upload rainbow
     glBindBuffer(GL_ARRAY_BUFFER, vboRainbow);
     glBufferData(GL_ARRAY_BUFFER, sizeof(rainbow), rainbow, GL_STATIC_DRAW);
+
+    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(verticec), verticec, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    // remember: do NOT unbind the EBO while a VAO is active as the bound element buffer object IS stored in the VAO; keep the EBO bound.
+    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+    glBindVertexArray(0);
+
 
     // Describe rainbow triangle:
     glBindVertexArray(vaoRainbow);
@@ -377,27 +374,26 @@ int main()
             break;
 
         case OBJ_4:
-  
-            shader = shaderDefault;
-            vertexData = vaoBlack;
-            // 
+            //shader = shaderDefault;
+            vertexData = VAO;
+           
         // render
         // ------
-            //glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        //glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
             
             glClear(GL_COLOR_BUFFER_BIT);
 
-            // draw our first triangle
+            // draw our first triangle (?)
+            //glBindVertexArray(VAO); 
             
-            glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
             //glDrawArrays(GL_TRIANGLES, 0, 6);
             glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-            // glBindVertexArray(0); // no need to unbind it every time 
+            //glBindVertexArray(0); // no need to unbind it every time 
 
             // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
             // -------------------------------------------------------------------------------
             glfwSwapBuffers(window);
-            glfwPollEvents();
+            //glfwPollEvents();
 
 
             /*shader = shaderAnimate;
@@ -409,6 +405,9 @@ int main()
             break;
 
         case OBJ_5:
+            // uncomment this call to draw in wireframe polygons.
+            //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
             shader = shaderDefault;
             vertexData = vaoBlue;
             glBindBuffer(GL_ARRAY_BUFFER, vboBlue);
